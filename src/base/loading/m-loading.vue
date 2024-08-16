5393862df1c7dd0d3fa6b439092a5f778ccb81d3
liangxuejie <liangxuejie704@163.com>
2024-08-16 14:06:12 +0800

    v-loading指令注册

---
+++ src/base/loading/m-loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loading">
+    <div class="loading-content">
+      <img width="24" height="24" src="./loading.gif">
+      <p class="desc">{{title}}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const title = ref('正在载入...')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  function setTitle(desc) {
+    title.value = desc
+  }
+</script>
+
+<style lang="scss" scoped>
+  .loading {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    .loading-content {
+      text-align: center;
+      .desc {
+        line-height: 20px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router'
 import App from './App.vue'
 import lazyPlugin from 'vue3-lazy'
 import loadingPic from '@/assets/images/default.png'
+import loadingDirective from '@/base/loading/directive'
 
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
@@ -13,4 +14,6 @@ app.use(router)
 app.use(lazyPlugin, {
   loading: loadingPic
 })
+app.directive('loading', loadingDirective)
+
 app.mount('#app')
